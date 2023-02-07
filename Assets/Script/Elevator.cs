6ee307dfaa2d40684f2eec8296965aea9ee5ebd9
tunglt <thanhtung12121212@gmail.com>
2023-02-07 17:14:09 +0700

    first commit

---
+++ Assets/Script/Elevator.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    private float m_TravelDistance = 0;
+    private float m_MaxTravelDistance = 15.0f;
+    private float m_Speed = 5.0f;
+
+    private Coroutine m_ReverseCoroutine;
+
+    private IEnumerator Start()
+    {
+        enabled= false;
+        yield return new WaitForSeconds(3.0f);
+        enabled= true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(m_TravelDistance >= m_MaxTravelDistance)
+        {
+            if(m_ReverseCoroutine == null)
+            {
+                m_ReverseCoroutine = StartCoroutine(nameof(ReverseElevator));
+            }
+        } else
+        {
+            float distanceStep = m_Speed * Time.fixedDeltaTime;
+            m_TravelDistance += Mathf.Abs(distanceStep);
+            transform.Translate(0, distanceStep, 0);
+        }
+    }
+
+    private IEnumerator ReverseElevator()
+    {
+        yield return new WaitForSeconds(3.0f);
+        m_TravelDistance = 0;
+        m_Speed = -m_Speed;
+        m_ReverseCoroutine= null;
+    }
+
+   
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody m_Rb;
+    private GameObject m_Elevator;
+    private float m_ElevatorOffsetY;
+    private void Awake()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+
+        
+        movement.Normalize();
+        
+        transform.Translate(movement * Time.deltaTime * speed);
+
+        m_Rb.MovePosition(m_Rb.position + movement * Time.fixedDeltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Elevator"))
+        {
+            m_Elevator = other.gameObject;
+            m_ElevatorOffsetY = transform.position.y - m_Elevator.transform.position.y; 
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Elevator"))
+        {
+            transform.parent = null;
+        }
+    }
+}
